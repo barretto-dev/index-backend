1ce9388f8233934d283998148f0ccd8297af94a8
Miguel Argolo <argolo.mb@gmail.com>
2026-05-05 12:32:43 -0300

    add live stream mode

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -6,5 +6,8 @@ router.get("/download", controller.downloadImagesZip);
 router.post("/download-and-save", controller.downloadAndSave);
 router.post("/prepare/start", controller.startPrepareFrames);
 router.post("/prepare/stop", controller.stopPrepareFrames);
+router.post("/record-ws/start", controller.startRecordWS);
+router.post("/record-ws/stop", controller.stopRecordWS);
+
 
 module.exports = router;
