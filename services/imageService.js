6a5596d9699f0803276ad05182a18538c354ba84
Miguel Argolo <argolo.mb@gmail.com>
2026-05-05 22:39:26 -0300

    Increase frame rate to 5 fps in WebSocket recording

--- services/imageService.js
+++ services/imageService.js
@@ -364,7 +364,7 @@ async function startRecordWS(wsUrl, outputDir) {
         "-analyzeduration", "1000000",
         "-i", "pipe:0",
         "-an",
-        "-vf", "fps=2",
+        "-vf", "fps=5",
         "-compression_level", "0",
         path.join(outputDir, "frame_%05d.png")
       ]);
